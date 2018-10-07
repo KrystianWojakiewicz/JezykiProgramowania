4e4c96707d502fdf0417d07f52bf81be645595f6
Krystian_Wojakiewicz <Krysh97@gmail.com>
2018-10-07 19:50:41 +0200

    Dodano funkcje zad2
    algorytm generowania liczb calkowitych

--- Lab01/src/Main.java
+++ Lab01/src/Main.java
@@ -5,19 +5,37 @@ public class Main {
         else return Liczba * silnia(Liczba -1);
     }
 
-    public static int Funkcja(int Liczba) {
+    public static int zad1(int Liczba) {
         int pi = 0;
 
         for(int i = 3; i < Liczba; i++) {
             pi = pi + silnia(i-2) - i * (silnia(i-2)/i);
         }
-        return pi -1;
+        return pi - 1;
     }
 
+    public static void zad2(int Liczba0, int depth) {
+        int Liczba1 = 0;
+        if(Liczba0 == 1) return;
+        if(Liczba0 % 2 == 0) {
+            Liczba1 = Liczba0 / 2;
+            System.out.println(depth + ", " + Liczba0 + " parzysta, " + Liczba1);
+            Liczba0 = Liczba1;
+        }
+        else if(Liczba0 % 2 != 0) {
+             Liczba1 = Liczba0 * 3 + 1;
+            System.out.println(depth + ", " + Liczba0 + " nieparzysta, " + Liczba1);
+            Liczba0 = Liczba1;
+
+        }
+        System.out.println("");
+        zad2(Liczba0, depth += 1);
+    }
 
     public static void main(String[] args) {
 
         int MojaLiczba = 10;
-        System.out.println("ilosc liczb pierwszych mniejszych lub rownych " + MojaLiczba + " wynosi " + Funkcja(MojaLiczba));
+        zad2(10, 1);
+        //System.out.println("ilosc liczb pierwszych mniejszych lub rownych " + MojaLiczba + " wynosi " + zad1(MojaLiczba));
     }
 }
